58017df2e00807ed7b4361f50933eee982b59516
Daniele Tricoli <eriol@mornie.org>
2018-11-19 01:44:11 +0100

    Add license header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+// Copyright © 2018 Daniele Tricoli <eriol@mornie.org>.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 #[macro_use]
 extern crate pest_derive;
 
--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -1,1 +1,6 @@
+// Copyright © 2018 Daniele Tricoli <eriol@mornie.org>.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 pub mod slice;
